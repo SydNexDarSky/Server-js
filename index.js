bf4e396f3784dbe102c09b328503551af1dc897f
Sydney205 <diekechi@gmail.com>
2026-04-07 21:37:14 +0100

    feat: created a simple server code

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000
+
+// Middlewares
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+	res.send('Server is running')
+})
+
+app.get('/api/test', (req, res) => {
+	res.json({ message: 'API is working'})
+})
+
+
+// Server 
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`)
+})
